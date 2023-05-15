5ec6df4e1da15824d62f7a1ee4ae1c40f84df0cc
Otis Jackson IV <ojacks7@lsu.edu>
2023-05-15 11:35:50 -0500

    Main C# Development file
    Mini Number Guessing program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class NG
+{
+    // Attributes
+    // Constructor
+    // Methods
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the C# Guessing Game !!!");
+
+        Random random = new Random();
+        int num = random.Next(1, 10);
+
+        Console.Write("\nGuess: ");
+
+        int gnum = Convert.ToInt32(Console.ReadLine());
+
+        while (gnum != num)
+        {
+            Console.Write("\nTry again : ");
+            gnum = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nYou guessed Correctly !");
+
+        Console.ReadKey();
+    }
+}
